2719229378b4a5fe07ad3c315cd82d58ed918d3e
Athalasyah Nabil <athalasyahnabil@gmail.com>
2025-09-23 21:17:51 +0800

    Add files via upload

---
+++ Model/Obligasi.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class Obligasi extends AsetInvestasi {
+    private double bunga;
+
+    public Obligasi(String nama, double nilai, double bunga) {
+        super(nama, nilai, "Obligasi");
+        this.bunga = bunga;
+    }
+
+    public double getBunga() { return bunga; }
+    public void setBunga(double bunga) { this.bunga = bunga; }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Bunga : " + bunga + "%");
+    }
+}
